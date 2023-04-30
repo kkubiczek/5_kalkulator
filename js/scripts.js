d878cf6f0ce8c554fd274e5ebf30fa1c8519321e
Krystian Kubiczek <kkubiczek00@gmail.com>
2023-04-30 13:32:19 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function calc() {
+    // console.log(this.innerText);
+
+    const buttonValue = this.innerText;
+
+    const resPlace = document.querySelector('#res');
+
+    if(buttonValue === 'C') {
+        resPlace.value = '';
+    } else if (buttonValue === '=') {
+        resPlace.value = eval(resPlace.value);
+    } else {
+        resPlace.value += buttonValue;
+    }
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((elem) => {
+
+    // console.log(elem);
+    elem.addEventListener('click', calc);
+
+});
